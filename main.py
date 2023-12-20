877d1f40465d4837344ad73c726ce6d70411f8f4
Mburg03 <mburgosgit003@gmail.com>
2023-12-20 14:25:33 -0600

    add database and table poblated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QLineEdit, QGridLayout, QPushButton, QCheckBox, QComboBox, QTableWidget, QMainWindow
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QLineEdit, QGridLayout, QPushButton, QCheckBox, QComboBox, QTableWidget, QMainWindow, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -24,15 +25,32 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
-    
+        self.resize(400, self.height())
+
+
 
     def load_data(self):
-        pass
+        connection = sqlite3.connect("./database.db")
+        result = connection.execute("SELECt * FROM students")
+        enumerated_result = enumerate(result)
+        self.table.setRowCount(0)
 
+        for row_number, row_data in enumerated_result:
+            self.table.insertRow(row_number)
+            self.table.setItem(row_number, 0, QTableWidgetItem(str(row_data[0])))
+            self.table.setItem(row_number, 1, QTableWidgetItem(str(row_data[1])))
+            self.table.setItem(row_number, 2, QTableWidgetItem(str(row_data[2])))
+            self.table.setItem(row_number, 3, QTableWidgetItem(str(row_data[3])))
+
+        connection.close()
+
+        
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
+main_window.load_data()
 main_window.show()
 sys.exit(app.exec())
 
